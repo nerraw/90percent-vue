c55ab07c60ead53b64ef188d10f5025faf7c245b
Warren Brodsky <warrenbrodsky@gmail.com>
2019-12-26 10:08:00 -0500

    Changed 'I think' to 'I am 90% confident'. Implemented selecting random subset from overall question pool

--- index.js
+++ index.js
@@ -11,11 +11,19 @@ const isValid = function(n) {
     }
     return true
 }
-const q1 = "What is the value of copper in the world's oceans (in USD)?"
-const q2 = "What is the average distance between Saturn and Uranus (in km)?"
-const q3 = "As of 2019, how many people over the age of 100 are alive in Russia?"
-const q4 = "Between 2000 and 2018, how many newborn males were named \"Owen\" in the U.S.?"
-const q5 = "As of 2019, how many active cell phones are in the world?"
+const q1 = "What is the value of copper in the world's oceans (in USD)?";
+const q2 = "What is the average distance between Saturn and Uranus (in km)?";
+const q3 = "As of 2019, how many people over the age of 100 are alive in Russia?";
+const q4 = "Between 2000 and 2018, how many newborn males were named \"Owen\" in the U.S.?";
+const q5 = "As of 2019, how many active cell phones are in the world?";
+const q6 = "What is the ratio between Pluto's diameter and the width of Australia, as measured from Sydeney to Perth?";
+var qdb = [];
+qdb.push(new q(q1, 109900000000000));
+qdb.push(new q(q2, 2872500000000));
+qdb.push(new q(q3, 20582));
+qdb.push(new q(q4, 81619));
+qdb.push(new q(q5, 9327053800));
+qdb.push(new q(q6, 0.5892));
 
 document.addEventListener("DOMContentLoaded", function() {
     app = new Vue({
@@ -32,7 +40,7 @@ document.addEventListener("DOMContentLoaded", function() {
         },
         methods: {
             submit() {
-                if (this.active >= 4) {
+                if (this.active >= this.questions.length - 1) {
                     this.state = "results"
                 } else {
                     this.active += 1;
@@ -58,6 +66,13 @@ document.addEventListener("DOMContentLoaded", function() {
             },
 
             newGame() {
+                dbcopy = qdb.slice();
+                this.questions = []
+                for (var i = 0; i < 3; i++) {
+                    var r = Math.floor(Math.random() * dbcopy.length);
+                    this.questions.push(dbcopy.splice(r, 1)[0]);
+                }
+
                 this.active = 0;
                 this.state = 'playing';
 
@@ -70,11 +85,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     });
 
-    app.questions[0] = new q(q1, 109900000000000);
-    app.questions.push(new q(q2, 2872500000000));
-    app.questions.push(new q(q3, 20582));
-    app.questions.push(new q(q4, 81619));
-    app.questions.push(new q(q5, 9327053800));
+
 
 
 
